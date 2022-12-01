04b3b10bcbe688c661f876d56cd96197580bd10f
AndreiYa <newminer@bk.ru>
2022-12-01 19:09:42 +0300

    fix: article timestamp

--- src/article/schema/article.schema.ts
+++ src/article/schema/article.schema.ts
@@ -3,7 +3,7 @@ import { Document } from 'mongoose';
 
 export type ArticleDocument = Article & Document;
 
-@Schema()
+@Schema({timestamps: true})
 export class Article {
   @Prop({type: String})
   media: string;
