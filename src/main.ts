109f2109466aa52ce7f57bbfd9789b0b4d2c8750
zidiks <zidiks228@gmail.com>
2023-03-20 11:05:52 +0300

    fix: ssl

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,12 @@ import {path} from 'app-root-path';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5001
-  // const httpsOptions = {
-  //   key: fs.readFileSync(`${path}/certs/properfumer.key`, 'utf-8'),
-  //   cert: fs.readFileSync(`${path}/certs/properfumer.crt`, 'utf-8'),
-  // };
+  const httpsOptions = {
+    key: fs.readFileSync(`${path}/certs/properfumer.key`, 'utf-8'),
+    cert: fs.readFileSync(`${path}/certs/properfumer.crt`, 'utf-8'),
+  };
 
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule, {httpsOptions})
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
   await app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
